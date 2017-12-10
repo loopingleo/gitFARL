840c4576d2e41d3a79bf6f858fb3fce120305955
loopingleo <leonhard.rassl@gmail.com>
2017-12-10 23:51:47 +0100

    quandl, matplotlib, data norm, plots -- basic stuff

--- FARL-python-playground.py
+++ FARL-python-playground.py
@@ -5,8 +5,34 @@ import datetime
 import pandas as pd
 import quandl
 import matplotlib.pyplot as plt
+import bs4 as bs
+import pickle
+import requests
 
-quandl.ApiConfig.api_key = "skVfzQSMxY-BnuR-7Zz3"
+import os
+#f = open(os.path.expanduser("~/PycharmProjects/gitFARL/quandlapikey.txt"))
+
+#api_key = open(os.path.expanduser("~/PycharmProjects/gitFARL/quandlapikey.txt"), "r").seek(-1,2).read()
+quandl.ApiConfig.api_key = your quandl api key
+
+
+
+def save_sp500_tickers():
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'}
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies',
+                        headers=headers)
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker)
+    with open("sp500tickers.pickle","wb") as f:
+        pickle.dump(tickers,f)
+
+    return tickers
+
 
 #irgendwas
 
@@ -38,3 +64,18 @@ list(AAPLdata.columns.values)
 AAPLdata.tot_revnu
 AAPLdata.per_end_date
 AAPLdata.sic_code
+
+AAPLdata.cash_flow_oper_activity.plot()
+AAPLdata.cash_flow_invst_activity
+
+FCF = AAPLdata.cash_flow_oper_activity - AAPLdata.cash_flow_invst_activity
+FCF.plot()
+FCFmargin = FCF / AAPLdata.tot_revnu /10
+FCFmargin.plot()
+
+FCFtoTA = FCF / AAPLdata.tot_asset
+FCFtoTA.plot()
+
+
+sp500_tickers = save_sp500_tickers()
+sp500_tickers[9]
