9e3fd7268e39f8ba12c11808302eff09cf3bd7a1
loopingleo <leonhard.rassl@gmail.com>
2017-12-10 20:01:12 +0100

    quandl, matplotlib, data norm, plots -- basic stuff

--- FARL-python-playground.py
+++ FARL-python-playground.py
@@ -1,10 +1,28 @@
 print("hello FARL")
 
+
 import datetime
 import pandas as pd
+import quandl
+import matplotlib.pyplot as plt
 
 
 #irgendwas
 
 oTime = datetime.datetime.now()
-print oTime.isoformat()
+print(oTime.isoformat())
+
+#quandl functions
+
+mydata = quandl.get("FRED/GDP")
+#normieren
+mydata_pct_change = mydata.pct_change()
+mydata_ind = (mydata["Value"] - mydata["Value"][1]) / mydata["Value"][1]*100.0 + 100.00
+
+
+plt.plot(mydata_ind)
+
+
+
+
+
